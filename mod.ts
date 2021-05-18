70b2be2dccf37b623abf8922a89762ed1adb51fb
agmeeder <ameeder@gmail.com>
2021-05-18 14:35:40 +0200

    Verbeteringen...

--- mod.ts
+++ mod.ts
@@ -7,6 +7,17 @@ import * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js'
 
 import { KeyValueObject, Street } from './types/mod.ts'
 
+declare global {
+  interface String {
+    insert(text: string, position: number): string;
+  }
+}
+
+String.prototype.insert = function (text: string, position: number) {
+  if (String(this).length <= position) return String(this)
+  return String(this).substring(0, position) + text + String(this).substring(position)
+}
+
 async function filterStreets(streetData: Array<KeyValueObject>) {
   return streetData.filter((street) => {
     return street["street_type"].trim().endsWith('Bestaand')
@@ -33,6 +44,7 @@ async function filterStreets(streetData: Array<KeyValueObject>) {
 async function convertStreets(streetData: Array<KeyValueObject>) {
   const result: Array<Street> = []
   streetData.forEach((street) => {
+    const pattern = /\D/g
     const date = street["date"] ? dateParse(street["date"].replace('T00:00:00', ''), 'yyyy-MM-dd') : new Date(1970,1,1)
 
     const newStreet: Street = {
@@ -40,16 +52,16 @@ async function convertStreets(streetData: Array<KeyValueObject>) {
       created: dateParse(street["created"], 'd-M-yyyy HH:mm'),
       changed: dateParse(street["changed"], 'd-M-yyyy HH:mm'),
       date: date,
-      latitude: Number(street["latitude"].replaceAll('.', '').insert('.', 2)),
-      longitude: Number(street["longitude"].replaceAll('.', '').insert('.', 1)),
+      latitude: Number(street["latitude"].replaceAll(pattern, '').insert('.', 2)),
+      longitude: Number(street["longitude"].replaceAll(pattern, '').insert('.', 1)),
       district: street["district"],
       import_x: Number(street["import_x"]),
       import_y: Number(street["import_y"]),
       title: street["title"],
       intro: street["intro"],
       body: street["body"],
-      benoemingsbesluit: street["benoemingsbesluit"],
-      periode: street["period"]
+      benoemingsbesluit: street["benoemingsbesluit"].replace('0,',''),
+      periode: street["period"].replace('0,','')
     };
 
     result.push(newStreet)
@@ -58,7 +70,7 @@ async function convertStreets(streetData: Array<KeyValueObject>) {
 }
 
 async function loadStreetnameData() {
-  const path = join('source', 'straatnamen.csv')
+  const path = join('source', 'straatnamen-van-rotterdam.csv')
   const file = await Deno.open(path)
   try {
     const bufReader = new BufReader(file)
